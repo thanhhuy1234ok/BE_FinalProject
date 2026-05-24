befe8a31f7db3e26ec136fe3a4eac99c8d8fd483
thanhhuy1234ok <nguyenvothanhhuy2002@gmail.com>
2026-05-24 16:57:33 +0700

    style: format code in AppController and AppService for consistency

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    test(): string {
+        return 'test api success';
+    }
 }
